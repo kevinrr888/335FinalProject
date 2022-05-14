c083f05c971d3bde5f1e496cb913ae1cd9aff1ba
Abbe Miller <33698904+AbbeMiller@users.noreply.github.com>
2022-05-14 17:34:09 -0400

    Fixed css file path

--- socialMediaServer.js
+++ socialMediaServer.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 app.set("views", path.resolve(__dirname, "templates"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname, "/css")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get('/', function(request, response){
     response.render("index");
